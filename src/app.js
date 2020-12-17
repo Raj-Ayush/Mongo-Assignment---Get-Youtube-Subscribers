964cca18ec28c6e2dfedc020326fde5b1f32e8b6
Your name here <rajayush8571@gmail.com>
2020-12-17 13:13:08 +0530

    1st commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,40 @@
 
 const express = require('express');
 const app = express()
-
+const subscriberModel = require('./models/subscribers')
 
 // Your code goes here
 
+app.get('/subscribers', async (req, res) => {
+    res.send(await subscriberModel.find());
+})
 
+app.get('/subscribers/names', async (req, res) => {
+    const fullResults = await subscriberModel.find();
+    const mappedResults = fullResults.map(doc => {
+        return {
+            name: doc.name,
+            subscribedChannel: doc.subscribedChannel
+        }
+    })
+    res.send(mappedResults);
+})
+
+app.get('/subscribers/:id', async (req, res) => {
+    const idTosearch = req.params.id;
+    console.log(idTosearch);
+    try{const doc = await subscriberModel.findOne({_id: idTosearch});
+        if(doc == null){
+            res.status(400).send({message: 'Id not found'});
+        }
+        else{
+            res.send(doc);
+        }
+    }
+    catch(e){
+        res.status(400).send({message: e.message});
+    }
+})
 
 
 
